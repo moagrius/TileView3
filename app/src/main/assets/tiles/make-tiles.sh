ce677a0f34835e7b8a90bd44383d83982facca35
Mike Dunn <mdunn@safaribooksonline.com>
2018-05-24 18:38:58 -0500

    removing tiles

--- app/src/main/assets/tiles/make-tiles.sh
+++
@@ -1,7 +0,0 @@
-for (( i=0; i <= 40; ++i))
-  do
-    for (( j=0; j <= 40; ++j))
-      do
-        convert -size 256x256 -pointsize 72 -gravity center -border 2x2 -quality 00 label:"${i}-${j}" "${i}_${j}.png"
-      done
-    done
