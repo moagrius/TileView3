a2122d9019509d4eb66cc6f1a4982a743a930688
Mike Dunn <mdunn@safaribooksonline.com>
2018-06-20 23:55:58 -0500

    readme

--- app/src/main/java/demo/Helpers.java
+++ app/src/main/java/demo/Helpers.java
@@ -1,9 +1,11 @@
 package demo;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 /**
  * @author Mike Dunn, 6/11/17.
@@ -12,14 +14,19 @@ import android.widget.TextView;
 public class Helpers {
 
   public static void populateLinearLayout(LinearLayout linearLayout, int quantity) {
+    populateLinearLayout(linearLayout, quantity, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT), "TextView #%1$d");
+  }
+
+  public static void populateLinearLayout(LinearLayout linearLayout, int quantity, LinearLayout.LayoutParams lp, String text) {
     Context context = linearLayout.getContext();
     for (int i = 0; i < quantity; i++) {
       TextView textView = new TextView(context);
-      textView.setText("Hi I'm TextView #" + i);
+      textView.setText(String.format(Locale.US, text, i));
       textView.setTextSize(30);
       textView.setPadding(100, 100, 100, 100);
-      textView.setBackgroundColor(Color.BLACK);
-      linearLayout.addView(textView);
+      textView.setBackgroundColor(0xFF383838);
+      textView.setTextColor(0xFFD8D8D8);
+      linearLayout.addView(textView, lp);
     }
   }
 
--- app/src/main/java/demo/ScrollViewDemoHorizontal.java
+++ app/src/main/java/demo/ScrollViewDemoHorizontal.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 import com.github.moagrius.tileview3.R;
 
@@ -17,7 +18,13 @@ public class ScrollViewDemoHorizontal extends AppCompatActivity {
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_demos_scrollview_horizontal);
-    LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearlayout);
-    Helpers.populateLinearLayout(linearLayout, 100);
+    LinearLayout linearLayout = findViewById(R.id.linearlayout);
+    Helpers.populateLinearLayout(
+        linearLayout, 100,
+        new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT),
+        "This TextView (number %1$d)\n" +
+            "is longer than other so that\n" +
+            "the horizontal ScrollView shows\n" +
+            "a more pager-like display");
   }
 }
